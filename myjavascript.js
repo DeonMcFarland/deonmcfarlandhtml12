81cb4b24094c500ef54fd078f98a4d71117ce741
DeonMcFarland <111295015+DeonMcFarland@users.noreply.github.com>
2022-08-30 21:20:56 -0400

    step 4

--- myjavascript.js
+++ myjavascript.js
@@ -3,6 +3,14 @@ myitem = document.getElementById("firsttest");
     myitem.addEventListener("click", onClick);
     myitem.style.fontsize="36px";
 myitem.style.color= "blue";
-    function onClick() {  
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+thebutton.addEventListener("click", onButtonClick);
+
+
+function onClick() {  
         myitem.style.color = "green";  
+    }
+    function onButtonClick() {  
+        otheritem.style.color = "yellow";  
     }
