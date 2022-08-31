c4be661309d5b0f7efde25eca7fe3b37fb6812e0
DeonMcFarland <111295015+DeonMcFarland@users.noreply.github.com>
2022-08-30 21:09:46 -0400

    steps 2 and 3

--- myjavascript.js
+++ myjavascript.js
@@ -1,7 +1,8 @@
 myitem = document.getElementById("firsttest");
 
     myitem.addEventListener("click", onClick);
-
+    myitem.style.fontsize="36px";
+myitem.style.color= "blue";
     function onClick() {  
         myitem.style.color = "green";  
     }
