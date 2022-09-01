3b22c6a6064e8e009d398c17ee1e3f588f2839e1
DeonMcFarland <111295015+DeonMcFarland@users.noreply.github.com>
2022-09-01 16:49:16 -0400

    yes

--- myjavascript.js
+++ myjavascript.js
@@ -23,15 +23,3 @@ function onChange() {
     newtext = myinput.value;  
     otheritem.innerHTML = newtext;  
 }
-
-mybigimage = document.getElementById("largeview");
-card1 = document.getElementById("soup1")
-card1.addEventListener("click", makeBigImage)
-mybigimage.addEventListener("click", hideBigImage)
-
-function makeBigImage() {
-    mybigimage.innerHTML = '' ;
-    bigimage = document.createElement("img");
-    bigimage.src ="images/mininons.jpg";
-    mybigimage.appendChild(bigimage);
-}
