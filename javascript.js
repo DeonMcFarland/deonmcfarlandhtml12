400361028d4ffc464744edccd9aebecee5824115
DeonMcFarland <111295015+DeonMcFarland@users.noreply.github.com>
2022-09-01 17:58:21 -0400

    thumbnail

--- javascript.js
+++ javascript.js
@@ -1,11 +1,26 @@
-mybigimage = document.getElementById("largeview");
-card1 = document.getElementById("soup1")
-card1.addEventListener("click", makeBigImage)
-mybigimage.addEventListener("click", hideBigImage)
+// mybigimage = document.getElementById("largeview");
+// card1 = document.getElementById("soup1")
+// card1.addEventListener("click", makeBigImage)
+// mybigimage.addEventListener("click", hideBigImage)
 
-function makeBigImage() {
-    mybigimage.innerHTML = '' ;
-    bigimage = document.createElement("img");
-    bigimage.src ="images/mininons.jpg";
-    mybigimage.appendChild(bigimage);
+// function makeBigImage() {
+//     mybigimage.innerHTML = '' ;
+//     bigimage = document.createElement("img");
+//     bigimage.src ="images/mininons.jpg";
+//     mybigimage.appendChild(bigimage);
+// }
+
+myMovie = document.getElementById("Movie");
+myImage = document.getElementById("minions");
+myMovie.addEventListener("click", changeImage);
+
+function changeImage() {
+    myImage.src = "images/mininons.jpg";
+    if (myImage.style.width != "800px") {
+        myMovie.style.display = "block";
+        myImage.style.width = "800px";
+    }
+    else {
+        myImage.style.width = "";
+    }
 }
