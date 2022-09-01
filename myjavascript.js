358fe108e6c20c4bddef41cf03adb6a3a05498d9
DeonMcFarland <111295015+DeonMcFarland@users.noreply.github.com>
2022-09-01 11:33:12 -0400

    thumb

--- myjavascript.js
+++ myjavascript.js
@@ -28,3 +28,10 @@ mybigimage = document.getElementById("largeview");
 card1 = document.getElementById("card1")
 card1.addEventListener("click", makeBigImage)
 mybigimage.addEventListener("click", hideBigImage)
+
+function makeBigImage() {
+    mybigimage.innerHTML = '' ;
+    bigimage = document.createElement("img");
+    bigimage.src ="images/mininons.jpg";
+    mybigimage.appendChild(bigimage);
+}
